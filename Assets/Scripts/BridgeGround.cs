be62f00f0d3098db92c2d30a3589e09d6f23f682
admin <richard.bota@khk.ee>
2017-12-26 22:15:42 +0200

    First Commit

---
+++ Assets/Scripts/BridgeGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeGround : MonoBehaviour
+{
+    [SerializeField] private float objectSpeed = 1;
+    [SerializeField] private float resetPosition = 26.53f;
+    [SerializeField] private float startPosition = -87.26f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update()
+	{
+	    transform.Translate(Vector3.right * (objectSpeed * Time.deltaTime));
+
+	    if (transform.localPosition.x >= resetPosition)
+	    {
+	        Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+	        transform.position = newPos;
+	    }
+	}
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : BridgeGround
+{
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    [SerializeField] private float speed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(Move(bottomPosition));
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    IEnumerator Move(Vector3 target)
+    {
+        while (Mathf.Abs((target - transform.localPosition).y) > 0.20f)
+        {
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * Time.deltaTime * speed;
+
+            yield return null;
+        }
+        print("Reached the target");
+
+        yield return new WaitForSeconds (0.5f);
+
+        Vector3 newTArget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+        StartCoroutine (Move (newTArget));
+    }
+}
